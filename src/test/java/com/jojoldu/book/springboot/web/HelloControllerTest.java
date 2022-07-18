08222ba7ed11b9f0a112bf35c1ac3e51dbfe14cc
myeongju00 <j51677767@gmail.com>
2022-07-19 01:35:43 +0900

    chapter5-1

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.jojoldu.book.springboot.web;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
