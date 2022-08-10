445e8d3d89de9907de01b9dbc00ea905a19d4e7d
myeongju00 <j51677767@gmail.com>
2022-08-11 02:10:00 +0900

    Chapter 8

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,48 +1,56 @@
 package com.jojoldu.book.springboot.web;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.hamcrest.Matchers.is;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.security.test.context.support.WithMockUser;
-//import org.springframework.test.web.servlet.MockMvc;
-//
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
 //@WebMvcTest(HelloController.class) //@RunWith
-//public class HelloControllerTest {
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @WithMockUser(roles = "USER")
-//    @Test
-//    void hello가_리턴된다() throws Exception {
-//        //given
-//        String hello = "hello";
-//
-//        //when
-//        mvc.perform(get("/hello"))
-//                //then
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(hello));
-//    }
-//
-//    @WithMockUser(roles = "USER")
-//    @Test
-//    void helloDto가_리턴된다() throws Exception {
-//        String name = "hello";
-//        int amount = 1000;
-//
-//        mvc.perform(
-//                get("/hello/dto")
-//                        .param("name", name)
-//                        .param("amount", String.valueOf(amount)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.name", is(name)))
-//                .andExpect(jsonPath("$.amount", is(amount))
-//        );
-//    }
-//}
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @WithMockUser(roles = "USER")
+    @Test
+    void hello가_리턴된다() throws Exception {
+        //given
+        String hello = "hello";
+
+        //when
+        mvc.perform(get("/hello"))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @WithMockUser(roles = "USER")
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount))
+        );
+    }
+}
